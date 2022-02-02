27f945aecc61b6288f244482bc1ad2c5dd4591f0
Naina Alex <naina.alex1@tvsmotor.com>
2022-02-02 18:48:17 +0530

    Update

--- Rule_based_NER.py
+++ Rule_based_NER.py
@@ -1,4 +1,12 @@
-import numpy
+import numpy as np
 import pandas as pd
 import nltk
 import spacy
+from nltk.tokenize import word_tokenize
+from nltk.tag import pos_tag
+
+ex = ['European authorities fined Google a record '+
+'$5.1 billion on Wednesday for abusing its power in '+
+'the mobile phone market and ordered the company to alter its practices']
+
+print(ex)
